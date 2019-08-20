ee7f7b25a0286320896002607b5412b42089ac62
Alex <asolosy@pagemasters.com.au>
2019-08-20 15:39:28 +1000

    no message

---
+++ static/precache-manifest.141e58e5523e1babac7e2d35c8865a21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "049367fad0e52a3d7b8d",
+    "url": "/css/main.chunk.css"
+  },
+  {
+    "revision": "049367fad0e52a3d7b8d",
+    "url": "/js/main.chunk.js"
+  },
+  {
+    "revision": "8afca781bf70484ea011",
+    "url": "/css/1.chunk.css"
+  },
+  {
+    "revision": "8afca781bf70484ea011",
+    "url": "/js/1.chunk.js"
+  },
+  {
+    "revision": "4fb3ceb36664b2d101a7",
+    "url": "/js/runtime~main.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "13decce88064ef4d25cd64a18ad287d5",
+    "url": "/index.html"
+  }
+];
